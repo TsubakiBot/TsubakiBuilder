9bc8b81793f4b35001c712464fe4c5cd91bb6489
aayush262 <aayushthakur262006@gmail.com>
2024-04-22 21:38:46 +0530

    fix: reading in manga instead of watching

--- app/src/main/java/ani/dantotsu/media/MediaInfoFragment.kt
+++ app/src/main/java/ani/dantotsu/media/MediaInfoFragment.kt
@@ -581,7 +581,7 @@ class MediaInfoFragment : Fragment() {
                         ).apply {
                             itemTitle.setText(R.string.social)
                             itemRecycler.adapter =
-                                MediaSocialAdapter(media.users!!)
+                                MediaSocialAdapter(media.users!!, type)
                             itemRecycler.layoutManager = LinearLayoutManager(
                                 requireContext(),
                                 LinearLayoutManager.HORIZONTAL,
