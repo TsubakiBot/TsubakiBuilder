Noirero/Lalapo

Fix extension interceptors receiving compressed responses (#10388)
Co-authored-by: stevenyomi <95685115+stevenyomi@users.noreply.github.com>

core/src/main/java/eu/kanade/tachiyomi/network/NetworkHelper.kt
@@ -2,6 +2,7 @@ package eu.kanade.tachiyomi.network
 
 import android.content.Context
 import eu.kanade.tachiyomi.network.interceptor.CloudflareInterceptor
+import eu.kanade.tachiyomi.network.interceptor.IgnoreGzipInterceptor
 import eu.kanade.tachiyomi.network.interceptor.UncaughtExceptionInterceptor
 import eu.kanade.tachiyomi.network.interceptor.UserAgentInterceptor
 import okhttp3.Cache
@@ -30,9 +31,10 @@ class NetworkHelper(
                     maxSize = 5L * 1024 * 1024, // 5 MiB
                 ),
             )
-            .addInterceptor(BrotliInterceptor)
             .addInterceptor(UncaughtExceptionInterceptor())
             .addInterceptor(UserAgentInterceptor(::defaultUserAgentProvider))
+            .addNetworkInterceptor(IgnoreGzipInterceptor())
+            .addNetworkInterceptor(BrotliInterceptor)
 
         if (preferences.verboseLogging().get()) {
             val httpLoggingInterceptor = HttpLoggingInterceptor().apply {

TsubakiBot/TsubakiBuilder

Co-Authored-By: stevenyomi <95685115+stevenyomi@users.noreply.github.com>

app/src/main/java/eu/kanade/tachiyomi/network/NetworkHelper.kt
@@ -7,6 +7,7 @@ import ani.dantotsu.settings.saving.PrefManager
 import ani.dantotsu.settings.saving.PrefName
 import com.lagradost.nicehttp.Requests
 import eu.kanade.tachiyomi.network.interceptor.CloudflareInterceptor
+import eu.kanade.tachiyomi.network.interceptor.IgnoreGzipInterceptor
 import eu.kanade.tachiyomi.network.interceptor.UncaughtExceptionInterceptor
 import eu.kanade.tachiyomi.network.interceptor.UserAgentInterceptor
 import okhttp3.Cache
@@ -35,8 +36,9 @@ class NetworkHelper(
                 ),
             )
             .addInterceptor(UncaughtExceptionInterceptor())
-            .addInterceptor(BrotliInterceptor)
             .addInterceptor(UserAgentInterceptor(::defaultUserAgentProvider))
+            .addInterceptor(IgnoreGzipInterceptor())
+            .addInterceptor(BrotliInterceptor)
 
         if (PrefManager.getVal(PrefName.VerboseLogging)) {
             val httpLoggingInterceptor = HttpLoggingInterceptor().apply {